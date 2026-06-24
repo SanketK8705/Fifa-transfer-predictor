0d08659bf2644f6fe4c0c63373fef4c887cb6984
Sanket Kumar Singh <sankets@Sankets-MacBook-Air.local>
2026-06-24 23:35:45 +0530

    Enable CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from config import Config
 
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app = Flask(
+        __name__,
+        template_folder='../templates',
+        static_folder='../static'
+    )
+
+    CORS(app)
+
     app.config.from_object(Config)
 
     db.init_app(app)
